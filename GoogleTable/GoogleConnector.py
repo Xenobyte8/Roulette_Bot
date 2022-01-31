66d8d3d1fe310037588a64227af1384f1633bf3d
MBerezovskiy <mberezovskiy@gmail.com>
2022-01-31 13:59:04 +0300

    dgf

--- GoogleTable/GoogleConnector.py
+++ GoogleTable/GoogleConnector.py
@@ -3,9 +3,9 @@ import pygsheets
 def GetTexts():
     auth = pygsheets.authorize()
     Sheet=auth.open('PidorTexts')
-    PidorTexts=Sheet.worksheet_by_title('Pidor')
-    Champions=Sheet.worksheet_by_title('Champions')
-    print (PidorTexts.get_all_values(include_tailing_empty=False, include_tailing_empty_rows=False))[0][0]
-    print ("ty pidor")
+    PidorTextsSheet=Sheet.worksheet_by_title('Pidor')
+    ChampionsSheet=Sheet.worksheet_by_title('Champions')
+    PidorTexts=PidorTextsSheet.get_all_values(include_tailing_empty=False, include_tailing_empty_rows=False))
+    print (PidorTexts)
 
 GetTexts()
