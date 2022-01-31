a9811478b36b779c5c26058a0be2282829548f35
MBerezovskiy <mberezovskiy@gmail.com>
2022-01-31 14:41:34 +0300

    sd

--- GoogleTable/GoogleConnector.py
+++ GoogleTable/GoogleConnector.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 import pygsheets
+import random
 
 def GetPidorTexts():
     auth = pygsheets.authorize('/home/mberezovskiy/Creds/CredentialsGoogle.json')
     Sheet=auth.open('PidorTexts')
     PidorTextsSheet=Sheet.worksheet_by_title('Pidor')
     ChampionsSheet=Sheet.worksheet_by_title('Champions')
-    PidorTexts=PidorTextsSheet.get_all_values(include_tailing_empty=False, include_tailing_empty_rows=False)
     PidorTexts=PidorTextsSheet.get_all_values(include_tailing_empty=False, include_tailing_empty_rows=False)
     if not PidorTexts[0]:
         PidorTexts=["Я хуй знает, кто поудалял все смехуечки, но пусть пидор будет "]
@@ -21,14 +21,10 @@ def GetChampions():
     Sheet=auth.open('PidorTexts')
     PidorTextsSheet=Sheet.worksheet_by_title('Pidor')
     ChampionsSheet=Sheet.worksheet_by_title('Champions')
-    PidorTexts=PidorTextsSheet.get_all_values(include_tailing_empty=False, include_tailing_empty_rows=False)
-    PidorTexts=PidorTextsSheet.get_all_values(include_tailing_empty=False, include_tailing_empty_rows=False)
-    if not PidorTexts[0]:
-        PidorTexts=["Я хуй знает, кто поудалял все смехуечки, но пусть пидор будет "]
     Champions=ChampionsSheet.get_all_values(include_tailing_empty=False, include_tailing_empty_rows=False)
     if not Champions[0]:
         Champions=["Я хуй знает, кто поудалял все смехуечки, но сегодня пидор "]
-
-    return(Champions)
+    Answer = Champions[random.randint(0, len(Champions)-1)]
+    return(Answer)
 
 print(GetChampions())
