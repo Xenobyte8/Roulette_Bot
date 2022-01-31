71a84f41d00b86f0ce46b719584d56bcca60db8a
MBerezovskiy <mberezovskiy@gmail.com>
2022-01-31 13:52:43 +0300

    xv

--- GoogleTable/GoogleConnector.py
+++ GoogleTable/GoogleConnector.py
@@ -5,7 +5,7 @@ def GetTexts():
     Sheet=auth.open('PidorTexts')
     PidorTexts=Sheet.worksheet_by_title('Pidor')
     Champions=Sheet.worksheet_by_title('Champions')
-    print (PidorTexts.get_all_values())
+    print (PidorTexts.get_all_values(include_tailing_empty=False))
     print ("ty pidor")
     print (str(PidorTexts.cell('A1')))
 
