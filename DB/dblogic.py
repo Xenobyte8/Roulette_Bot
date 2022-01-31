8eeb13320c249216e84bb888b8a067998940a1f6
MBerezovskiy <mberezovskiy@gmail.com>
2022-01-31 11:14:16 +0300

    dadada

--- DB/dblogic.py
+++ DB/dblogic.py
@@ -115,7 +115,7 @@ def GetResult(DB_Connector, ChatId):
     ChallengeId=IsExist[0]
     mycursor.execute("select PersonName, Score, TelegaId from Challenger where ChallengeId=" + str(ChallengeId))
     ChallengeInfo = mycursor.fetchall()
-    CurrentPidorID=str(mycursor.execute("select CurrentPidor from Challenge where ChatId=" + str(ChatId)))
+    CurrentPidorID=mycursor.execute("select CurrentPidor from Challenge where ChatId=" + str(ChatId)).fetchone()
     CurrentPidorName=str("select PersonName from Challenger where ChallengeID="+ str(ChallengeId) +"and TelegaID = (select CurrentPidor from Challenge where ChatId=" + str(ChatId)+")")
     Result=(ChallengeInfo, CurrentPidorID)
     print (Result)
